c61a8747c38f5e0b8aa1d30dcc0cd1893b7a3bac
Saparbek28 <46023456+Saparbek28@users.noreply.github.com>
2019-06-26 21:12:03 +0600

    first commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +23,73 @@ namespace NetHW3
     /// </summary>
     public partial class MainWindow : Window
     {
+        private TcpClient _tcpClient;
+        Thread listenThread;
+        Thread sendThread;
         public MainWindow()
         {
             InitializeComponent();
+            _tcpClient = new TcpClient();
+
+
+        }
+
+        private void ConnectButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _tcpClient.ConnectAsync(IPAddress.Parse(ipTextBox.Text), int.Parse(portTextBox.Text));
+                listenThread = new Thread(ListenToServer);
+                listenThread.Start(_tcpClient);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+
+        }
+
+        private void ListenToServer(object obj)
+        {
+            TcpClient tcpServer = obj as TcpClient;
+            tcpServer.Client.Send(Encoding.ASCII.GetBytes("Saparbek"));
+            byte[] buffer = new byte[4 * 1024];
+            while (true)
+            {
+                var receivedSize = tcpServer.Client.Receive(buffer);
+                TextToChat(Encoding.UTF8.GetString(buffer, 0, receivedSize));
+            }
+        }
+
+        private void TextToChat(string text)
+        {
+            Dispatcher.Invoke(new ThreadStart(() => chatTextBox.AppendText(text + "\n")));
+        }
+
+        private void SendButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                sendThread = new Thread(SendMessage);
+                sendThread.Start(_tcpClient);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void SendMessage(object obj)
+        {
+            TcpClient tcpServer = obj as TcpClient;
+            string buffer = "";
+            Dispatcher.Invoke(new ThreadStart(() => buffer = messageTextBox.Text));
+            _tcpClient.Client.Send(Encoding.ASCII.GetBytes(buffer), SocketFlags.None);
+        }
+
+        private void CloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
